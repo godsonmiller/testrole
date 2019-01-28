cc46da81b70c6cd3287ee732a031ce8bdd330f70
godsonmiller <godsanmiller@gmail.com>
2019-01-27 18:40:59 -0800

    Add files via upload

---
+++ src/app/roleview/roleview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roleview',
+  templateUrl: './roleview.component.html',
+  styleUrls: ['./roleview.component.css']
+})
+export class RoleviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
