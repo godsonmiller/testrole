6aa891cb7a7c10f8e4ea810964068bd11d4d17cf
godsonmiller <godsanmiller@gmail.com>
2019-01-28 23:02:36 -0800

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //constructor() { }
+   constructor(public dialog: MatDialog) {}
+   openDialog(): void {
+    const dialogRef = this.dialog.open(SignupComponent, {
+      width: '250px'}
+    });
 
   ngOnInit() {
   }
+
+}
+
+@Component({
+  selector: 'signup.component',
+  templateUrl: 'signup.component.html',
+})
+export class SignupComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<SignupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
 
 }
